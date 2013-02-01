67ec4ffb879ec9180b65a206d499eabfc122dad4
Ravi Rajamani <ravibeta@hotmail.com>
2013-01-31 16:08:50 -0800

    Update oddone.py

--- oddone.py
+++ oddone.py
@@ -1,5 +1,5 @@
 def oddone(text5):
  fdist5 = FreqDist(text5)
-  Vocabulary = sorted([w for w in set(text5) if len(w) > 7 and fdist5[w] > 7])
+  vocabulary = sorted([w for w in set(text5) if len(w) > 7 and fdist5[w] > 7])
   return vocabulary[:50]
   
