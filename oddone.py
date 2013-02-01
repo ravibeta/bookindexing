6bf95d3a3d5f3839af2999453f9cdbabbccc4f0f
Ravi Rajamani <ravibeta@hotmail.com>
2013-01-31 16:39:27 -0800

    Update oddone.py

--- oddone.py
+++ oddone.py
@@ -1,5 +1,12 @@
-def oddone(text5):
+def indexUniGram(text):
  fdist5 = FreqDist(text5)
-  vocabulary = sorted([w for w in set(text5) if len(w) > 7 and fdist5[w] > 7])
-  return vocabulary[:50]
+ fdist5.hapaxes()
+ vocabulary = sorted([w for w in set(text5) if len(w) > 7 and fdist5[w] > 7])  
+ return vocabulary[:50]
+ 
+ def indexBiGram(text):
+  V = text.collocations()
+  fdist6 = FreqDist([text.count(w) for w in V])
+  v = sorted([w for w in set(text) if len(w) > 7 and fdist6[w] > 7])
+  return v[:50]
   
