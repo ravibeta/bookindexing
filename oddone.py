7247e80e1acb90368c8afeff6decae95031bae7a
Ravi Rajamani <ravibeta@hotmail.com>
2013-02-01 15:36:33 -0800

    Update oddone.py

--- oddone.py
+++ oddone.py
@@ -1,5 +1,5 @@
 def indexUniGram(text, commonwords):
- fdist5 = FreqDist(text5, commonwords)
+ fdist5 = FreqDist(text5)
  fdist5.hapaxes()
  vocabulary = sorted([w for w in set(text5) if ( w not in commonwords || (len(w) > 7 and fdist5[w] > 7))])  
  return vocabulary[:50]
@@ -7,6 +7,13 @@ def indexUniGram(text, commonwords):
  def indexBiGram(text):
   V = text.collocations()
   fdist6 = FreqDist([text.count(w) for w in V])
-  v = sorted([w for w in set(text) if ( w not in commonwords || (len(w) > 7 and fdist6[w] > 7)])
-  return v[:50]
+  vocabulary = sorted([w for w in set(text) if ( w not in commonwords || (len(w) > 7 and fdist6[w] > 7)])
+  return vocabulary[:50]
   
+def IndexUniGramContextBased(text, commonwords):
+ words = sorted([w for w in set(text) if (w not in commonwords)]);
+ fdist7 = FreqDist([w for w in text.concordance([v for v in words]))
+ vocabulary = sorted([w for w in set(words) if ( fdist7[w] > 7))
+ return vocabulary[:50]
+ 
+ 
