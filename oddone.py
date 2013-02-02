00edb1085e08c662b6b6d5e54c6b88611549b816
Ravi Rajamani <ravibeta@hotmail.com>
2013-02-01 17:10:17 -0800

    Update oddone.py

--- oddone.py
+++ oddone.py
@@ -4,7 +4,7 @@ def indexUniGram(text, commonwords):
  vocabulary = sorted([w for w in set(text5) if ( w not in commonwords || (len(w) > 7 and fdist5[w] > 7))])  
  return vocabulary[:50]
  
- def indexBiGram(text):
+def indexBiGram(text, commonwords):
   V = text.collocations()
   fdist6 = FreqDist([text.count(w) for w in V])
   vocabulary = sorted([w for w in set(text) if ( w not in commonwords || (len(w) > 7 and fdist6[w] > 7)])
@@ -16,4 +16,13 @@ def IndexUniGramContextBased(text, commonwords):
  vocabulary = sorted([w for w in set(words) if ( fdist7[w] > 7))
  return vocabulary[:50]
  
+def IndexConditionalFreqDist(text, commonwords):
+  vocabulary = IndexUniGram(text, commonwords)
+  cfd = nltk.ConditionalFreqDist(
+            (category, word)
+             for category in text.categories()
+             for word in text.words(categories=category))
+  l = len(text)
+  category = [text[1:l/3], text[l/3+1:2*l/3], text[2*1/3+1, l]]
+  return cfd.tabulate(conditions = category, samples = vocabulary)
  
