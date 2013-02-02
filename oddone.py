17535e2d28faee48c6c84becf99f927e480d08d0
Ravi Rajamani <ravibeta@hotmail.com>
2013-02-01 20:37:20 -0800

    Update oddone.py

--- oddone.py
+++ oddone.py
@@ -10,19 +10,31 @@ def indexBiGram(text, commonwords):
   vocabulary = sorted([w for w in set(text) if ( w not in commonwords || (len(w) > 7 and fdist6[w] > 7)])
   return vocabulary[:50]
   
-def IndexUniGramContextBased(text, commonwords):
- words = sorted([w for w in set(text) if (w not in commonwords)]);
+def indexUniGramContextBased(text, commonwords):
+ words = sorted([w for w in set(text) if (w not in commonwords)])
  fdist7 = FreqDist([w for w in text.concordance([v for v in words]))
  vocabulary = sorted([w for w in set(words) if ( fdist7[w] > 7))
  return vocabulary[:50]
  
-def IndexConditionalFreqDist(text, commonwords):
+def indexConditionalFreqDist(text, commonwords):
   vocabulary = IndexUniGram(text, commonwords)
   cfd = nltk.ConditionalFreqDist(
             (category, word)
              for category in text.categories()
              for word in text.words(categories=category))
   l = len(text)
-  category = [text[1:l/3], text[l/3+1:2*l/3], text[2*1/3+1, l]]
+  category = [text[1:l/3], text[(l/3)+1:2*l/3], text[(2*1/3)+1, l]]
   return cfd.tabulate(conditions = category, samples = vocabulary)
+  
+  def indexUnstructuredDocument(text, commonwords)
+   return indexUniGram(text, commonwords)
+# view the document as sections
+# pick out salient words based on fequency
+# use the wordnet to find the common ancestor
+# if the ancestor changes on a rolling basis such as in a novel
+# adhust the section size and list one word per section.
+  
+  def indexStructuredDocument(text, commonwords)
+    return indexUniGram(textHeading, commonwords).Intersect(indexUniGram(textSubHeadings, commonwords))
+
  
